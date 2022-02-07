0b3dc98115997f9ec7a4a919d9dc80999582b150
Gabriel gmdl <oogmdl@gmail.com>
2022-02-07 19:26:44 -0400

    first heroku deploy

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + 'dist/angular-tutorial'));
+app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', (req, res) => {
-    res.sendFile(__dirname + 'dist/angular-tutorial/index.html');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + 'dist/index.html');
 });
 
 app.listen(PORT, () => {
-    console.log('Server @ port: ' + PORT);
+    console.log('Listening on port: ' + PORT);
 });
